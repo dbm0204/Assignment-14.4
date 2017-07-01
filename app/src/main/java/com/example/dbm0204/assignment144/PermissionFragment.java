232a0115e72c434526199d947382b2f06710a5fb
dbm0204 <dbm0204@gmail.com>
2017-07-01 13:47:25 +0530

    Project Finished

---
+++ app/src/main/java/com/example/dbm0204/assignment144/PermissionFragment.java
@@ -0,0 +1,21 @@
+package com.example.dbm0204.assignment144;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by dbm0204 on 7/1/17.
+ */
+
+public class PermissionFragment extends Fragment {
+    @Nullable
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup conatiner, Bundle savedInsatnceState){
+        return inflater.inflate(R.layout.fragment_main,null);
+    }
+}
